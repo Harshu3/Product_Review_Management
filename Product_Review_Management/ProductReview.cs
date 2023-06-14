84dc6952313042393e56a5091a32710c065c6b32
Harshita <chandaliyaharshita222@gmail.com>
2023-06-14 22:04:55 +0530

    Create product review class

---
+++ Product_Review_Management/ProductReview.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Review_Management
+{
+    internal class ProductReview
+    {
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public int Rating { get; set; }
+        public string Review { get; set; }
+        public bool IsLike { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {ProductId} userId: {UserId} rating: {Rating} review: {Review} isLike: {IsLike}";
+        }
+    }
+}
--- Product_Review_Management/Program.cs
+++ Product_Review_Management/Program.cs
@@ -7,6 +7,24 @@ namespace Product_Review_Management
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Product Review Management using LINQ Problem!");
+            List<ProductReview> list = new List<ProductReview>()
+            {
+                new ProductReview(){ProductId = 1, UserId = 1, Review = "good", IsLike = true, Rating = 80},
+                new ProductReview(){ProductId = 2, UserId = 2, Review = "bad", IsLike = false, Rating = 30},
+                new ProductReview(){ProductId = 3, UserId = 4, Review = "average", IsLike = true, Rating = 50},
+                new ProductReview(){ProductId = 2, UserId = 3, Review = "good", IsLike = true, Rating = 90},
+                new ProductReview(){ProductId = 4, UserId = 5, Review = "bad", IsLike = false, Rating = 20},
+                new ProductReview(){ProductId = 1, UserId = 6, Review = "good", IsLike = true, Rating = 80}
+            };
+            DisplayProducts(list);
+        }
+
+        public static void DisplayProducts(List<ProductReview> list)
+        {
+            foreach (ProductReview product in list)
+            {
+                Console.WriteLine(product);
+            }
         }
     }
 }
